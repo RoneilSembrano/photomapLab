91116ab91f64c4ab1822ca90646a657c1458932e
roneil <roneilsembrano@gmail.com>
2020-03-20 19:45:09 -0700

    configured the photo button still working on segue

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,117 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+/* -- Comment -- */
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    /* ---- TODO: Create mapView outlet*/
+
+    
+    
+    // Store picked image
+    var pickedImage: UIImage!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
+    }
+    
+    
+    
+    /* ----- TODO: Instantiate UIImagePicker after camera button tapped */
+  
+    @IBAction func onCameraTap(sender: Any) {
+        selectPhoto()
+    }
+
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        onCameraTap(sender: self)
+    }
+    
+    
+    
+    /* ----- TODO: Retrieve coordinates from LocationsViewController   */
+    
+    
+    
+    /* ----- TODO: add pin to the map */
+    
+    
+    
+    /* ----- TODO: Customize mapview to add custom map notations */
+    
+    
+    
+    
+    // Instantiate Image Picker and set delegate to this view controller
+    func selectPhoto() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        // Present camera, if available
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            imagePicker.sourceType = .camera
+            
+            // Present photo library
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            imagePicker.sourceType = .photoLibrary
+            // Present imagePicker source type (either camera or library)
+        }
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        // Get the image captured by the UIImagePickerController
+        let _ = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as! UIImage
+        let editedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        pickedImage = editedImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true)
+        performSegue(withIdentifier: "tagSegue", sender: self)
+        
+    }
+
+}
+
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
